0aae9485cf93d3a7f73106ad2c523b229adeedae
Keith Moulton <keithm@xmos.com>
2021-02-03 07:41:52 -0500

    fixed copyright year

--- test/shared/testing/testing_print.c
+++ test/shared/testing/testing_print.c
@@ -1,4 +1,4 @@
-// Copyright 2021 XMOS LIMITED. This Software is subject to the terms of the 
+// Copyright 2020 XMOS LIMITED. This Software is subject to the terms of the 
 // XMOS Public License: Version 1
 
 #include "testing.h"
