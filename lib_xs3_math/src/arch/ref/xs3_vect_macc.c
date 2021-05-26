31ab6142eecd151e44de8ad8d1180dd4f89d6af8
Aaron Stewart <aaron.stewart@xmos.com>
2021-05-26 14:20:44 -0400

    Forgot to update the C versions of the macc/nmacc functions

--- lib_xs3_math/src/arch/ref/xs3_vect_macc.c
+++ lib_xs3_math/src/arch/ref/xs3_vect_macc.c
@@ -18,12 +18,14 @@ headroom_t xs3_vect_s16_macc(
     const int16_t b[],
     const int16_t c[],
     const unsigned length,
-    const right_shift_t a_shr)
+    const right_shift_t acc_shr,
+    const right_shift_t bc_shr)
 {
 
     for(int k = 0; k < length; k++){
+        acc[k] = vlashr16(acc[k], acc_shr);
         const vpu_int16_acc_t tmp = vlmacc16(0, b[k], c[k]);
-        acc[k] = vladd16(acc[k], vlsat16(tmp, a_shr));
+        acc[k] = vladd16(acc[k], vlsat16(tmp, bc_shr));
     }
 
     return xs3_vect_s16_headroom(acc, length);
@@ -34,12 +36,14 @@ headroom_t xs3_vect_s16_nmacc(
     const int16_t b[],
     const int16_t c[],
     const unsigned length,
-    const right_shift_t a_shr)
+    const right_shift_t acc_shr,
+    const right_shift_t bc_shr)
 {
 
     for(int k = 0; k < length; k++){
+        acc[k] = vlashr16(acc[k], acc_shr);
         const vpu_int16_acc_t tmp = vlmacc16(0, b[k], c[k]);
-        acc[k] = vlsub16(acc[k], vlsat16(tmp, a_shr));
+        acc[k] = vlsub16(acc[k], vlsat16(tmp, bc_shr));
     }
 
     return xs3_vect_s16_headroom(acc, length);
@@ -58,6 +62,7 @@ headroom_t xs3_vect_s32_macc(
 {
 
     for(int k = 0; k < length; k++){
+        acc[k] = vlashr32(acc[k], acc_shr);
         const int32_t B = vlashr32(b[k], b_shr);
         const int32_t C = vlashr32(c[k], c_shr);
         acc[k] = vladd32(acc[k], vlmul32(B, C));
@@ -78,6 +83,7 @@ headroom_t xs3_vect_s32_nmacc(
 {
 
     for(int k = 0; k < length; k++){
+        acc[k] = vlashr32(acc[k], acc_shr);
         const int32_t B = vlashr32(b[k], b_shr);
         const int32_t C = vlashr32(c[k], c_shr);
         acc[k] = vlsub32(acc[k], vlmul32(B, C));
