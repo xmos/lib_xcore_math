6de9cc72c137b036314a2b38248a640978d0c739
Aaron Stewart <aaron.stewart@xmos.com>
2021-06-08 16:52:56 -0400

    Removes support for XS3_MATH_VECTOR_TAIL_SUPPORT.
    - Configurable support for the XS3_MATH_VECTOR_TAIL_SUPPORT option has been removed.
    - Functions that previously supported it now assume the tail is to be supported.
    - Any functions that do not support vector tails must explicitly declare so in their documentation.

--- lib_xs3_math/api/xs3_math_conf.h
+++ lib_xs3_math/api/xs3_math_conf.h
@@ -32,55 +32,6 @@
 #endif
 
 
-/**
- * @page compile_time_options Compile Time Options
- * 
- * @par Disable Vector Tail Support
- * 
- *     XS3_MATH_VECTOR_TAIL_SUPPORT
- * 
- * Most of the `lib_xs3_math` API functions take vectors as inputs or outputs. By default there are no restrictions on 
- * the lengths of these vectors (other than being non-zero).
- * 
- * The XS3 vector registers are `256` bits. Optimal performance is obtained when the length of a vector is a multiple of 
- * the vector register length. The table below indicates the bit depth (bits-per-element; BPE) and number of 
- * elements-per-vector (EPV) for frequently-used data types. Vector elements in excess of a natural boundary (i.e. 
- * `vector_length mod EPV`) are often referred to in this library as the "tail" of a vector.
- * 
- * Usually checking for vector tails does not cost more than a few thread cycles per API function call when there is no 
- * tail. Where there is a tail, handling the tail within an API function is often significantly more expensive than 
- * dealing with a full EPV elements. It is, for example, considerably faster to multiply two 16-bit vectors of length 
- * `16` (using the VPU) than two vectors of length `1`.
- * 
- * The main drawback of allowing vector tails is that it entails a sometimes significant increase in the code memory 
- * footprint. To that end, where an application can guarantee that all vector lengths are an integer multiple of EPV for 
- * that element type, support for vectors tails in some API functions can be disabled by compiling with the macro 
- * `XS3_MATH_VECTOR_TAIL_SUPPORT` defined to be zero.
- * 
- * If vector tail support is disabled, the behavior of API functions where tails are implied by vector lengths is 
- * undefined.
- * 
- * @note Currently, most functions do not yet respect this option.
- * 
- * Element Type  | BPE | EPV 
- * ------------- | --- | ---
- * int8_t        |  8  | 32
- * int16_t       | 16  | 16
- * int32_t       | 32  |  8
- * complex_s32_t | 64  |  4
- * 
- * By default, tails are supported (1).
- * 
- */
- #ifndef XS3_MATH_VECTOR_TAIL_SUPPORT
-
-/**
- * See @ref compile_time_options for details.
- */
- #define XS3_MATH_VECTOR_TAIL_SUPPORT (1)
- #endif
-
-
 
 /**
  * @page compile_time_options Compile Time Options
