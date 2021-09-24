754da3864e28a72c48067b43b30e3b67e3a75c6d
Aaron Stewart <aaron.stewart@xmos.com>
2021-09-24 16:14:14 -0400

    CI update

--- .github/scripts/build_tests_x86.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-cmake -B build.x86
-cmake --build build.x86
--- .github/scripts/run_tests_x86.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-./build.x86/test/scalar_tests/scalar_tests -s
-./build.x86/test/vect_tests/vect_tests     -s
-./build.x86/test/bfp_tests/bfp_tests       -s
-./build.x86/test/fft_tests/fft_tests       -s
-./build.x86/test/filter_tests/filter_tests -s
