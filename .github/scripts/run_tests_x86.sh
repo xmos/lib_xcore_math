e3d45955f339fbb5529624ba2bf18a24d07393dd
Aaron Stewart <aaron.stewart@xmos.com>
2021-09-24 15:01:10 -0400

    Made x86 test runs quieter

--- .github/scripts/run_tests_x86.sh
+++ .github/scripts/run_tests_x86.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
 
-./build.x86/test/scalar_tests/scalar_tests
-
-./build.x86/test/vect_tests/vect_tests
-
-./build.x86/test/bfp_tests/bfp_tests
-
-./build.x86/test/fft_tests/fft_tests
-
-./build.x86/test/filter_tests/filter_tests
+./build.x86/test/scalar_tests/scalar_tests -s
+./build.x86/test/vect_tests/vect_tests     -s
+./build.x86/test/bfp_tests/bfp_tests       -s
+./build.x86/test/fft_tests/fft_tests       -s
+./build.x86/test/filter_tests/filter_tests -s
