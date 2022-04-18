a0a674ab52ed0e050f12ed07796e55bb4629f6ce
Aaron Stewart <aaron.stewart@xmos.com>
2022-04-18 14:00:36 -0400

    [fixes #88] Changes xs3_api.h to conditionally define macros.
    This is necessary becaues the e.g. C_API macro is used in other libraries now, and unconditionally defining these macros causes inclusion-order-dependent errors.

--- lib_xs3_math/api/xs3_api.h
+++ lib_xs3_math/api/xs3_api.h
@@ -16,28 +16,38 @@
  */
 
 
-#if defined(__cplusplus)
-#  define EXTERN_C extern "C"
-#else
-#  define EXTERN_C 
-#endif //__cplusplus
+#ifndef EXTERN_C
+#  if defined(__cplusplus)
+#    define EXTERN_C extern "C"
+#  else
+#    define EXTERN_C 
+#  endif //__cplusplus
+#endif
 
 /// Used to indicate a function uses the C function ABI
-#define C_API EXTERN_C
+#ifndef C_API
+#  define C_API EXTERN_C
+#endif
 /// Used to indicate a struct is a C type
-#define C_TYPE EXTERN_C
+#ifndef C_TYPE
+#  define C_TYPE EXTERN_C
+#endif
 
 /// Many API functions require word or double word alignment
-#ifdef __xcore__
-#  define ALIGNMENT(N)  __attribute__((aligned (N)))
-#else
-#  define ALIGNMENT(N)
+#ifndef ALIGNMENT
+#  ifdef __xcore__
+#    define ALIGNMENT(N)  __attribute__((aligned (N)))
+#  else
+#    define ALIGNMENT(N)
+#  endif
 #endif
 
 /// (on xcore) Force variable to word alignment
-#define WORD_ALIGNED   ALIGNMENT(4)
+#ifndef WORD_ALIGNED
+#  define WORD_ALIGNED   ALIGNMENT(4)
+#endif
 
 /// (on xcore) Force variable to double word alignment
-#define DWORD_ALIGNED  ALIGNMENT(8)
-
-//
+#ifndef DWORD_ALIGNED
+#  define DWORD_ALIGNED  ALIGNMENT(8)
+#endif
