860d5c457178540c4f203f791e1be93a67b723c0
Aaron Stewart <aaron.stewart@xmos.com>
2022-04-22 11:20:59 -0400

    Updates copyright/license notice in source files.

--- lib_xs3_math/doc/src/reference/notes.h
+++ lib_xs3_math/doc/src/reference/notes.h
@@ -1,3 +1,5 @@
+// Copyright 2021-2022 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 // This file exists as a compatibility work-around between vanilla Doxygen and 
 // sphinx+breathe+doxygen.
