e246dbea89a860eac9601ec5062d9add24b17586
Aaron Stewart <aaron.stewart@xmos.com>
2022-10-17 15:32:49 -0400

    Fixes style issue with docs build.
    Removed custom CSS file from sphinx doc build to match style of other docs.

--- doc/conf.py
+++ doc/conf.py
@@ -82,8 +82,6 @@ html_theme_options = {
     "sidebar_hide_name": True,
 }
 
-html_css_files = ['custom_style.css']
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
